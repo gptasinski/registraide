7fcd23bee080d6809a7f43252b632c34a54a14f8
gptasinski <g.ptasinski78@gmail.com>
2016-04-25 15:17:33 -0500

    Add authorize before hooks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,22 @@ module ApplicationHelper
     session[:id] != nil
   end
 
+  def authorize
+    unless authenticated?
+      redirect_to login_path
+    end
+  end
+
+  def authorized_to_edit
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+      flash[:message] = "So sorry, you don't have authorization for that."
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def authorized_to_register
+
+  end
+
 end
